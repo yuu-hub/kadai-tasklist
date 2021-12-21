4f935184e96e571887dd982f88974423ba567bdd
y.sasamori <info.yuu09@gmail.com>
2021-12-21 12:02:04 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
     
   resources :tasks
 end
